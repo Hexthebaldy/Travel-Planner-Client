2e179220b65b60df3966ac3a8a3a642d91fc2869
Hexthebaldy <7thwesk@gmail.com>
2025-03-23 00:13:50 +0800

    1

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './UserProfile.css'
 
 interface UserPreferences{
-    preferences: string[];
+    preferredDestination: string[];
     travelStyle: string;
     budget: string;
 };
@@ -14,8 +14,78 @@ interface UserData{
 };
 
 const UserProfile: React.FC = ()=>{
+    const [user,setUser] = useState<UserData>(
+        {
+            name:'Yang',
+            email:'giveMeOffer@gmail.com',
+            preferences:{
+                preferredDestination:['Paris','New York','Tokyo'],
+                travelStyle:'Adventure',
+                budget:'medium'
+            }
+        }
+    );
+
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [formData,setFormData] = useState<UserData>(user);
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>): void =>{
+        const {name,value} = e.target;
+        setFormData({
+            ...formData,
+            [name]:value
+        });
+    };
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>): void =>{
+        e.preventDefault();
+        setUser(formData);
+        setIsEditing(false);
+    }
+
     return(
         <div className="user-profile">
+            {!isEditing ? (
+                <div className="profile-view">
+                    <div className="profile-header">
+                        <h2>{user.name}</h2>
+                        <button onClick={()=>setIsEditing(true)}>Edit Profile</button>
+                    </div>
+                    <div className="profile-details">
+                        <p><strong>Email: </strong> {user.email}</p>
+                        <p><strong>Preference: </strong>{user.preferences.preferredDestination.join('   ')}</p>
+                        <p><strong>Travel Style: </strong>{user.preferences.travelStyle}</p>
+                        <p><strong>Budget: </strong>{user.preferences.budget}</p>
+                    </div>
+                </div>
+            ):( // is editing
+                <form action="" className="profile-form"
+                onSubmit = {handleSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input 
+                            type="text"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange} 
+                        />
+                    </div>
+                    <div className="form-actions">
+                        <button type="submit">Save</button>
+                        <button type="button" onClick={() => setIsEditing(false)}>Cancel</button>
+                    </div>
+                </form>
+            )
+        }
         </div>
     )
 };
