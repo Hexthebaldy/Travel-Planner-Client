68bc53c0bb18c22a3e06cf5e548353ae5993b1b5
Hexthebaldy <7thwesk@gmail.com>
2025-03-22 16:58:23 +0800

    set router rules

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar: React.FC = ()=>{
+    return(
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/">
+                    Start Your Journey
+                </Link>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/plans" className="nav-link">
+                            MyPlans
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/profile" className="nav-link">
+                            Profile
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
