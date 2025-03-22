c9e8b6012906e25d248373ffafd51a40df1bb838
Hexthebaldy <7thwesk@gmail.com>
2025-03-22 16:33:49 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Home from './pages/Home';
 import './styles.css';
 
 const App: React.FC = ()=>{
   return(
     <Router>
       <div className="App">
-
+        <main className='container'>
+            <Routes>
+                <Route path = '/' element={<Home />}></Route>
+            </Routes>
+        </main>
       </div>
     </Router>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home:React.FC = ()=>{
+    return (
+        <div>
+            <h1>AI Travel Planner</h1>
+            <p>Let's use AI travel planner create your travel plan</p>
+        </div>
+    )
+}
+
+export default Home;
