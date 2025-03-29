5d2b8787a5cfcadcd09c7fa9fe60fc3980bb72bf
Hexthebaldy <7thwesk@gmail.com>
2025-03-29 23:45:30 +0800

    add style

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,7 +4,6 @@ import UserProfile from '../components/UserProfile';
 const Profile: React.FC = ()=>{
     return(
         <div className="profile-page">
-            <h1>User Profile</h1>
             <UserProfile/>
         </div>
     )
